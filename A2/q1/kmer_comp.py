84e03a7934b27ca3854b0d2b08e8d2f2cdb2576b
e48huang <e48huang@uwaterloo.ca>
2025-02-25 14:44:36 -0500

    Made the ambiguous bases work properly as expected

--- A2/q1/kmer_comp.py
+++ A2/q1/kmer_comp.py
@@ -21,13 +21,22 @@ is the output since AA has 2, AC has 1, etc.
 """
 
 from Bio import SeqIO
+from typing import List
 import argparse
 import logging
 import os
 import sys
 
 
-def get_sequence(input_file):
+def get_sequence(input_file) -> str:
+    """Parses the input file path to get the fasta sequence.
+
+    Args:
+        input_file (str): Path to input file.
+
+    Returns:
+        str: The fasta sequence
+    """
     # parse the FASTA file
     with open(input_file, 'r') as handle:
         # Parse the FASTA file
@@ -44,7 +53,6 @@ def map_kmer_to_index(kmer):
         kmer (str): Given some k-mer, maps it to its proper 4^k index.
     """
     
-    # TODO: List of indices for repeats?
     # assuming we have AAAA = 0, etc., we can define this as:
     # A = 0, C = 1, G = 2, T = 3 and we have that each position represents
     # the power to multiply from, so if we have XYZ then this would result in
@@ -57,7 +65,7 @@ def map_kmer_to_index(kmer):
         'G': 2,
         'T': 3
     }
-    
+
     # reverse the kmer which will help with finding the index more easily
     kmer = kmer[::-1]
     
@@ -68,21 +76,84 @@ def map_kmer_to_index(kmer):
     return index
 
 
-def get_kmer_frequency_array(seq, k):
+def gen_all_kmers(kmer) -> List[str]:
+    """Given a kmer string, produce a list of kmers that remove its ambiguity.
+
+    Args:
+        kmer (str): The kmer string to map and produce all kmers
+
+    Returns:
+        List[str]: Reduces the ambiguous degenerative pairs to their proper pairing.
+    """
+   
+    # this approach assumes that every single base is evenly likely given
+    # this is taken from this wikipedia page on IUPAC notation:
+    # https://en.wikipedia.org/wiki/Nucleic_acid_notation
+    kmer_mapping = {
+        'A': ['A'],
+        'C': ['C'],
+        'G': ['G'],
+        'T': ['T'],
+        'U': ['T'], # treat uracil exactly the same as thymine
+        'W': ['A', 'T'],
+        'S': ['C', 'G'],
+        'M': ['A', 'C'],
+        'K': ['G', 'T'],
+        'R': ['A', 'G'],
+        'Y': ['C', 'T'],
+        'B': ['C', 'G', 'T'],
+        'D': ['A', 'G', 'T'],
+        'H': ['A', 'C', 'T'],
+        'V': ['A', 'C', 'G'],
+        'N': ['A', 'C', 'G', 'T']
+    }
+
+    kmers = ['']
+
+    for i in range(len(kmer)):
+        kmers = [
+            kmer_prefix + next_char
+            for kmer_prefix in kmers
+            for next_char in kmer_mapping[kmer[i]]
+        ]
+
+    return kmers
+
+
+def get_kmer_frequency_array(seq, k) -> List[float | int]:
     """Given some DNA sequence, returns the kmer frequency array.
 
     Args:
         seq (str): The given DNA sequence seq.
         k (int): The kmer length.
+
+    Returns:
+        List[float] | List[int]: A list of frequencies that are either partial weights
+            or if there are no ambiguities, all ints
     """
+    # populate the frequencies of size 4^k
     kmer_frequencies = [0 for _ in range(4 ** k)]
 
     # we should iterate n - k + 1 times, from [0, n - k] as our start range
     for i in range(len(seq) - k + 1):
         kmer = seq[i:i + k]
-        kmer_index = map_kmer_to_index(kmer)
-        logging.debug(f'Found kmer and its index: {kmer}, {kmer_index}')
-        kmer_frequencies[kmer_index] += 1
+
+        # then, transform each kmer to a list of kmers that could represent it
+        # after which, we will evenly split the weighting of the kmers' frequency
+        # across all possible kmers
+        kmers_possible = gen_all_kmers(kmer)
+        logging.debug(f'Found all possible kmers : {kmers_possible}')
+
+        for kmer_possible in kmers_possible:
+            kmer_index = map_kmer_to_index(kmer_possible)
+
+            # weighting to be int if there is no ambiguity
+            weight = 1 / len(kmers_possible) if len(kmers_possible) != 1 else 1
+
+            kmer_frequencies[kmer_index] += weight
+            logging.debug(
+                f'{kmer_possible}: Its index at weight {weight}, {kmer_index}'
+            )
 
     return kmer_frequencies
 
@@ -118,3 +189,11 @@ if __name__ == '__main__':
 
     with open(output_file_path, 'w') as output_file:
         output_file.write(' '.join(map(str, kmer_frequency)))
+
+    # if there is debug set, ensure that the total frequency is what you would expect it to be
+    if args.debug:
+        total_freq = 0
+        for freq in kmer_frequency:
+            total_freq += freq
+        assert total_freq == len(seq) - k + 1 # the total frequency should be n - k + 1
+        logging.debug('Total frequency matches properly!')
